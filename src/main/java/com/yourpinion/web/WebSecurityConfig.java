d0c0bc71fb49f4b44a18a7a82e6dd0f009889816
Elzbieta Maczka <umbrela95@o2.pl>
2021-06-16 23:24:17 +0200

    userRepository and userService added

--- src/main/java/com/yourpinion/web/WebSecurityConfig.java
+++ src/main/java/com/yourpinion/web/WebSecurityConfig.java
@@ -1,16 +1,21 @@
 package com.yourpinion.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -19,11 +24,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
+        auth
+           .userDetailsService(userDetailsService)
+           .passwordEncoder(getPasswordEncoder());
+
+        /*auth.inMemoryAuthentication()
             .passwordEncoder(getPasswordEncoder())
             .withUser("username@example.com")
             .password(getPasswordEncoder().encode("pass123"))
-            .roles("USER");
+            .roles("USER");*/
     }
 
     @Override
