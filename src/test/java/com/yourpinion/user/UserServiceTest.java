5d0cb36fba112d7ab241435f7c7cbb608aa31c13
Elzbieta Maczka <umbrela95@o2.pl>
2021-06-19 23:23:36 +0200

    password encoding test

--- src/test/java/com/yourpinion/user/UserServiceTest.java
+++ src/test/java/com/yourpinion/user/UserServiceTest.java
@@ -1,7 +1,19 @@
 package com.yourpinion.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
+
+    @Test
+    public void shouldGenerateEncryptedPassword() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "Password123";
+        String encodedPassword = encoder.encode(rawPassword);
+
+        assertNotEquals(rawPassword, encodedPassword);
+    }
 
 }
