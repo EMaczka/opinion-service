b9ef093fd1c0afd9914233c7c994484740e64e68
Elzbieta Maczka <umbrela95@o2.pl>
2021-06-28 23:29:23 +0200

    get product page

--- src/main/java/com/yourpinion/product/ProductController.java
+++ src/main/java/com/yourpinion/product/ProductController.java
@@ -2,12 +2,17 @@ package com.yourpinion.product;
 
 import com.yourpinion.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
 
 @Controller
 public class ProductController {
@@ -16,12 +21,20 @@ public class ProductController {
     private ProductRepository productRepository;
 
     @GetMapping("/products")
-    public String getProducts(ModelMap model) {
-        return "product";
-    }
+    public String getProducts(ModelMap model) { return "product"; }
 
     @GetMapping("/products/{productId}")
-    public String getProduct(@PathVariable Long productId) {
+    public String getProduct(@PathVariable Long productId, HttpServletResponse response, ModelMap modelMap) throws IOException {
+        Optional<Product> productOptional = productRepository.findById(productId);
+
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+            modelMap.put("product", product);
+        } else {
+            response.sendError(HttpStatus.NOT_FOUND.value(), "Product with id " + productId + " not found");
+            return "product";
+        }
+
         return "product";
     }
 
