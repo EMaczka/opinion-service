b01eb58862812dd3e8e1014d8f5ff51ab9a49e12
Elzbieta Maczka <umbrela95@o2.pl>
2021-09-10 19:08:35 +0200

    resolving bug with switch button

--- src/main/java/com/yourpinion/feature/Feature.java
+++ src/main/java/com/yourpinion/feature/Feature.java
@@ -9,7 +9,6 @@ import javax.persistence.*;
 
 @Getter
 @Setter
-@ToString
 @Entity
 public class Feature {
 
--- src/main/java/com/yourpinion/product/Product.java
+++ src/main/java/com/yourpinion/product/Product.java
@@ -13,6 +13,7 @@ import java.util.Set;
 @Setter
 @Entity
 public class Product {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
